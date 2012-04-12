5f5e54b8f73ae2251040296dde25010abf44e7ff
enroscar <enroscar@3e0dc993-7aae-5b17-d3f7-95cf743d6c5c>
2012-04-12 16:01:29 +0000

    debug flags

--- trunk/eclipse-project/src/com/stanfy/DebugFlags.java
+++ trunk/eclipse-project/src/com/stanfy/DebugFlags.java
@@ -1,5 +1,7 @@
 package com.stanfy;
 
+import com.stanfy.views.BuildConfig;
+
 /**
  * Debug flags. They are switched according to build configuration.
  * @author Roman Mazur - Stanfy (http://www.stanfy.com)
@@ -24,27 +26,27 @@ public final class DebugFlags {
   public static final boolean DEBUG_IMAGES = false; // @debug.images@
 
   /** GUI debug flag. */
-  public static final boolean DEBUG_GUI = true; // @debug.gui@
+  public static final boolean DEBUG_GUI = BuildConfig.DEBUG; // @debug.gui@
 
   /** API debug flag. */
-  public static final boolean DEBUG_API = true; // @debug.api@
+  public static final boolean DEBUG_API = BuildConfig.DEBUG; // @debug.api@
 
   /** Utilities debug flag. */
   public static final boolean DEBUG_UTILS = false; // @debug.utils@
 
   /** Debug service flag. */
-  public static final boolean DEBUG_SERVICES = true; // @debug.services@
+  public static final boolean DEBUG_SERVICES = BuildConfig.DEBUG; // @debug.services@
 
   /** Debug parser flag. */
   public static final boolean DEBUG_PARSER = false; // @debug.parser@
 
   /** Debug location flag. */
-  public static final boolean DEBUG_LOCATION = true; // @debug.location@
+  public static final boolean DEBUG_LOCATION = BuildConfig.DEBUG; // @debug.location@
 
   /** Debug C2DM. */
-  public static final boolean DEBUG_C2DM = true; // @debug.c2dm@
+  public static final boolean DEBUG_C2DM = BuildConfig.DEBUG; // @debug.c2dm@
 
   /** Debug stats. */
-  public static final boolean DEBUG_STATS = true; // @debug.stats@
+  public static final boolean DEBUG_STATS = BuildConfig.DEBUG; // @debug.stats@
 
 }
